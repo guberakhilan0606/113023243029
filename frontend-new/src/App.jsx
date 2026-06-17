c8eed2a10df4fc60430c9c5874fe6cdf1e4b2752
akhilan16062006-cloud <ntgakhilan16062006@gmail.com>
2026-06-17 12:30:50 +0530

    Integrate logger in app startup

--- frontend-new/src/App.jsx
+++ frontend-new/src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { Log } from "./middleware/logger";
+
 function App() {
+
+  useEffect(() => {
+    Log(
+      "frontend",
+      "info",
+      "component",
+      "Campus notifications page loaded"
+    );
+  }, []);
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Campus Notifications</h1>
